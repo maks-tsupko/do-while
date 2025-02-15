ab893c01410fb3c0977313f849bdb0fdfa0f9658
Maksim <koramba921@mail.ru>
2025-02-15 13:45:05 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        // задача 1
+        System.out.println("задача 1");
+        int salary = 15000;
+        int total = 0;
+        while (total <= 2_490_000) {
+            total = total + salary;
+            System.out.println(total);
 
+        }
     }
-
 
 }
